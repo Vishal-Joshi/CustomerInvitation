4ca43f3f3b2cddaa2d7d43a0017d7918fc4b4236
vishal <joshivishal2010@gmail.com>
2017-10-01 23:34:23 +0100

    Added failing test for eligible customer service

--- src/main/java/com/intercom/test/customerinvitation/services/EligibleCustomerService.java
+++ src/main/java/com/intercom/test/customerinvitation/services/EligibleCustomerService.java
@@ -1,14 +1,32 @@
 package com.intercom.test.customerinvitation.services;
 
+import com.intercom.test.customerinvitation.entities.Coordinate;
 import com.intercom.test.customerinvitation.entities.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class EligibleCustomerService {
 
-    public List<Customer> printEligibleCustomers(double radiusInKms) {
+    private final DistanceCalculatorService distanceCalculatorService;
+    private final CustomerFileReaderService customerFileReaderService;
+
+    @Autowired
+    public EligibleCustomerService(DistanceCalculatorService distanceCalculatorService,
+                                   CustomerFileReaderService customerFileReaderService) {
+        this.distanceCalculatorService = distanceCalculatorService;
+        this.customerFileReaderService = customerFileReaderService;
+    }
+
+    public List<Customer> printEligibleCustomers(String path, Coordinate source, double radiusInKms) {
+        try {
+            List<Customer> customers = customerFileReaderService.readFile(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
--- src/test/groovy/com/intercom/test/customerinvitation/services/EligibleCustomerServiceSpecification.groovy
+++ src/test/groovy/com/intercom/test/customerinvitation/services/EligibleCustomerServiceSpecification.groovy
@@ -1,20 +1,34 @@
 package com.intercom.test.customerinvitation.services
 
+import com.intercom.test.customerinvitation.entities.Coordinate
 import com.intercom.test.customerinvitation.entities.Customer
 import spock.lang.Specification
 
 
 class EligibleCustomerServiceSpecification extends Specification {
 
+    CustomerFileReaderService mockCustomerFileReaderService = Mock(CustomerFileReaderService)
+    DistanceCalculatorService mockDistanceCalculatorService = Mock(DistanceCalculatorService)
+
     def "service should return eligible customers lying within passed radius"() {
         given:
-        EligibleCustomerService eligibleCustomerService = new EligibleCustomerService()
+        EligibleCustomerService eligibleCustomerService = new EligibleCustomerService(mockDistanceCalculatorService, mockCustomerFileReaderService)
+        String path = "path"
         double radiusInKms = 100
+        //London 51.5074° N, 0.1278° W
+        def customer1 = Customer.builder().name("Vishal").userId("1").latitude(53.339428).longitude(-6.257664).build()
+        def customer2 = Customer.builder().name("Vishal").userId("1").latitude(53.349428).longitude(-6.267664).build()
+        def customerFarOffInLondon = Customer.builder().name("Vishal").userId("1").latitude(51.5074).longitude(0.1278).build()
+        List<Customer> expectedListOfCustomers = [customer1, customer2, customerFarOffInLondon]
+        //Dublin 53.339428, -6.257664.
+        Coordinate dublin = Coordinate.builder().latitude(53.339428).longitude(-6.257664).build()
 
         when:
-        List<Customer> customers = eligibleCustomerService.printEligibleCustomers(radiusInKms)
+        List<Customer> customers = eligibleCustomerService.printEligibleCustomers(path, dublin, radiusInKms)
 
         then:
+        1 * mockCustomerFileReaderService.readFile(_ as String) >> expectedListOfCustomers
+        3 * mockDistanceCalculatorService.calculateDistanceInKilometers(dublin, _ as Coordinate)
         customers != null
         !customers.isEmpty()
     }
