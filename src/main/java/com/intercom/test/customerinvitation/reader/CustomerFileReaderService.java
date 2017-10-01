c6cf56fbe23c68aec4fed5435b52a9ff264fd7c5
vishal <joshivishal2010@gmail.com>
2017-10-01 19:46:03 +0100

    Add tests for invalid json string

--- src/main/java/com/intercom/test/customerinvitation/reader/CustomerFileReaderService.java
+++ src/main/java/com/intercom/test/customerinvitation/reader/CustomerFileReaderService.java
@@ -2,13 +2,16 @@ package com.intercom.test.customerinvitation.reader;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intercom.test.customerinvitation.entity.Customer;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class CustomerFileReaderService {
 
     public List<Customer> readFile(String path) throws IOException {
@@ -18,11 +21,12 @@ public class CustomerFileReaderService {
                 .map(line -> {
                     try {
                         return objectMapper.readValue(line, Customer.class);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    } catch (IOException exception) {
+                        log.error("Could not de-serialise json string to java object", exception);
                         return null;
                     }
                 })
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 }
--- src/test/groovy/com/intercom/test/customerinvitation/reader/CustomerFileReaderServiceSpecification.groovy
+++ src/test/groovy/com/intercom/test/customerinvitation/reader/CustomerFileReaderServiceSpecification.groovy
@@ -15,5 +15,20 @@ class CustomerFileReaderServiceSpecification extends Specification {
         then:
         customers != null
         !customers.isEmpty()
+        customers.size() == 5
+    }
+
+    def "service should skip null customer objects which are invalid customer json string"() {
+        given:
+        CustomerFileReaderService customerFileReaderService = new CustomerFileReaderService()
+        String path = this.getClass().getClassLoader().getResource("customer-test-file-invalid.txt").getPath()
+
+        when:
+        def customers = customerFileReaderService.readFile(path)
+
+        then:
+        customers != null
+        !customers.isEmpty()
+        customers.size() == 4
     }
 }
